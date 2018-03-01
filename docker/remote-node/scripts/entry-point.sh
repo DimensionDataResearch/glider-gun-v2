f7e00cd8756787b36c9a8ae24b516a4d973aafec
Adam Friedman <tintoy@tintoy.io>
2018-03-02 08:07:09 +1100

    Don't try to be clever; just send pushd / popd output to /dev/null.

--- docker/remote-node/scripts/entry-point.sh
+++ docker/remote-node/scripts/entry-point.sh
@@ -9,36 +9,34 @@ RKE_MANIFEST_FILE=/state/cluster.yml
 
 COMMAND=${1:-}
 
-alias gtfo=/dev/null
-
 deploy() {
     echo 'Creating hosts and installing Docker...'
 
-    pushd /deploy > gtfo
+    pushd /deploy > /dev/null
 
     terraform apply -auto-approve -state=$TF_STATE_FILE -lock=false
     terraform refresh -state=$TF_STATE_FILE -lock=false
     generate-rke-config.py --terraform-state-file $TF_STATE_FILE --ssh-key-file=$SSH_SECURE_KEY_FILE --cluster-manifest-file=$RKE_MANIFEST_FILE
 
-    popd > gtfo # /deploy
+    popd > /dev/null # /deploy
 
     echo 'Creating Kubernetes cluster...'
 
-    pushd /state > gtfo
+    pushd /state > /dev/null
 
     rke up
 
-    popd > gtfo # /state
+    popd > /dev/null # /state
 }
 
 destroy() {
     echo 'Destroying hosts...'
 
-    pushd /deploy > gtfo
+    pushd /deploy > /dev/null
 
     terraform destroy -force -state=$TF_STATE_FILE -lock=false
 
-    popd > gtfo # /deploy
+    popd > /dev/null # /deploy
 }
 
 # Irritatingly, we can't change permissions on a bind-mount
