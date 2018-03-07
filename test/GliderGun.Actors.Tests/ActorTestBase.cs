57d420c696526df058cb501a43e94544c270e6bb
Adam Friedman <tintoy@tintoy.io>
2018-03-07 14:55:32 +1100

    Add test project and base class for actor test suites.

---
+++ test/GliderGun.Actors.Tests/ActorTestBase.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit.Abstractions;
+
+using TestKit = Akka.TestKit.Xunit2.TestKit;
+
+namespace GliderGun.Actors.Tests
+{
+    /// <summary>
+    ///     The base class for actor test suites.
+    /// </summary>
+    public abstract class ActorTestBase
+        : TestKit
+    {
+        /// <summary>
+        ///     Create a new <see cref="ActorTestBase"/>.
+        /// </summary>
+        /// <param name="testOutput">
+        ///     Output for the current test.
+        /// </param>
+        protected ActorTestBase(ITestOutputHelper testOutput)
+            : base(output: testOutput)
+        {
+        }
+    }
+}
